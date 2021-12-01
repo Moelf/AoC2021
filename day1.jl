b8ca7dbc51f6283838d5dbfff8239a997791a631
Moelf <proton@jling.dev>
2021-12-01 00:53:36 -0500

    day1 p2

--- day1.jl
+++ day1.jl
@@ -1,3 +1,11 @@
 const ints = parse.(Int, readlines("./input1"))
 
-@show count(>(0), diff(ints))
+p1 = count(>(0), diff(ints))
+
+println("Day1: $p1")
+
+p2 = count(4:lastindex(ints)) do idx
+    ints[idx-3] < ints[idx]
+end
+
+println("Day1 p2: $p2")
