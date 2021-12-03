f3b99bca1052122a5208600fb75489b23f8f6a21
Moelf <proton@jling.dev>
2021-12-03 10:34:20 -0500

    day 3

---
+++ day3.jl
@@ -0,0 +1,23 @@
+rows = map(eachline("./input3")) do line
+    parse.(Bool, collect(line))
+end
+rowidxs = eachindex(rows[1])
+mode(rows, j, op = >=) = op(sum(r->r[j], rows), length(rows)÷2)
+f(x) = parse(Int, replace(bitstring(x), " "=>""); base=2)
+f(x::Vector) = f(BitVector(x))
+
+### part 1
+γ = [mode(rows, j) for j in rowidxs]
+ϵ = .~(γ) # bit flips
+println("p1: ",  prod(f, (γ, ϵ)))
+
+### part 2
+part2(rows, j, op) = filter(r -> r[j]==mode(rows, j, op), rows)
+let oxygen = rows, CO2 = rows
+    for j in rowidxs
+        length(oxygen)==1 && length(CO2)==1 && break
+        oxygen = part2(oxygen, j, >=)
+         CO2 = part2(CO2, j, <)
+    end
+    println("p2: ",  prod(f∘only, (oxygen, CO2)))
+end
