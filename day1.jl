cfc09311f1ef20e52ecd0122b8e0d19e46dd03ed
Moelf <proton@jling.dev>
2021-12-02 22:00:50 -0500

    golf a bit

--- day1.jl
+++ day1.jl
@@ -1,10 +1,10 @@
 const ints = parse.(Int, readlines("./input1"))
 
-p1 = count(>(0), diff(ints))
+p1 = sum(>(0), diff(ints))
 
 println("Day1: $p1")
 
-p2 = count(4:lastindex(ints)) do idx
+p2 = sum(4:lastindex(ints)) do idx
     ints[idx-3] < ints[idx]
 end
 
