4c3e1720596d4d047bbc7e81ebb383b89d500d34
Matthew Zjack <vardamir@protonmail.ch>
2021-12-02 21:22:59 -0600

    add dumb soln for day 1 part 1

--- day1.ml
+++ day1.ml
@@ -1,4 +1,10 @@
-(* calls f num_incr ic_head *)
+(* memory-efficient solution
+ * uses continuation-passing style
+ *)
+let input = open_in "./input1"
+(* calculates num increments as well as first depth
+ * calls f num_incr first_depth
+ *)
 let rec k_num_incr ic f =
   match input_line ic with
         exception End_of_file -> f 0 None
@@ -11,6 +17,23 @@ let rec k_num_incr ic f =
                     f (num_incr + if this_depth < next_depth then 1 else 0) (Some this_depth)
           )
 
-let input = open_in "./input1"
 let _ = k_num_incr input (fun num_incr _ -> (print_int num_incr; print_endline ""))
+
+
+
+(* golfy solution *)
+let input = open_in "./input1"
+(* reads entire file into list *)
+let rec get_depths ic =
+  match input_line ic with
+        exception End_of_file -> []
+      | line -> (int_of_string line)::(get_depths ic)
+
+let depths = get_depths input
+(* fold by keeping & updating a tuple:
+ * current num increments, previous depth
+ *)
+let (result, _) = List.fold_left (fun (num_incr, prev_depth) this_depth -> ((num_incr + if this_depth > prev_depth then 1 else 0), this_depth)) (0, List.hd depths) (List.tl depths)
+
+let _ = print_int result; print_endline ""
 
