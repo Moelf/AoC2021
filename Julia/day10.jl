c4317558edebd9bd849b37ff4d658ed791299c3a
Moelf <proton@jling.dev>
2021-12-10 04:12:42 -0500

    day10

--- Julia/day10.jl
+++ Julia/day10.jl
@@ -1,29 +1,28 @@
 function checker(line, part2)
-    match = Dict('(' => ')', '[' => ']', '{' => '}', '<' => '>')
-    smap = Dict('(' => 1, '[' => 2, '{' => 3, '<' => 4,
-        ')' => 3, ']' => 57, '}' => 1197, '>' => 25137)
-    record = Char[]
     res1 = res2 = 0
+    match = Dict('(' => ')', '[' => ']', '{' => '}', '<' => '>')
+    smap =  Dict('(' => 1,   '[' => 2,   '{' => 3,   '<' => 4,
+                 ')' => 3,   ']' => 57,  '}' => 1197,'>' => 25137)
+    stack = Char[]
     for t in line
         if t ∈ "([{<"
-            push!(record, t)
-        elseif t == match[last(record)]
-            pop!(record)
+            push!(stack, t)
+        elseif t == match[last(stack)]
+            pop!(stack)
         else
             res1 = smap[t]
-            @goto hack
+            @goto corrupted
         end
     end
-    foreach(reverse(record)) do r
+    foreach(reverse(stack)) do r
         res2 *= 5
         res2 += smap[r]
     end
     push!(part2, res2)
-    @label hack
+    @label corrupted
     res1
 end
 
 p2 = Int[]
-t = map(l->checker(l, p2), eachline("../input10"))
-println("P1: ", sum(t))
-println("P2: ", sort(p2)[end÷2+1])
+println("P1: ", sum(l -> checker(l, p2), eachline("../input10")))
+println("P2: ", sort(p2)[end ÷ 2 + 1])
